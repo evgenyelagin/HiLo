4b3901929aceb234fe9bf68e934bef760c3962c8
evgenyelagin <anykeysky@gmail.com>
2020-11-24 17:15:47 +0300

    add comment

--- src/ru/HiLo.java
+++ src/ru/HiLo.java
@@ -1,10 +1,13 @@
 package ru;
 
-import java.util.Scanner;
+import java.util.Scanner; // импорт класса сканер для обработки данных введенных с клавиатуры
 
 public class HiLo {
+    // Игра - угадай число, да комменты будут на русском - пишу для себя :)
     public static void main(String[] args) {
+        // создаем класс сканер - для принятия значений с клавиатуры
         Scanner scan = new Scanner(System.in);
+        // генерация случайного числа через класс Мас.рандом - фишка в том, что от 0.0 до 0.9, поэтому и * на 100
         int theNumber = (int) (Math.random() * 100 + 1);
     }
 }
