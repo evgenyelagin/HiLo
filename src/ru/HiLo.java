9f50a3230da440080bbea6b06d2d68d2f6dbf7aa
evgenyelagin <anykeysky@gmail.com>
2020-11-24 17:00:27 +0300

    Initial commit

---
+++ src/ru/HiLo.java
@@ -0,0 +1,10 @@
+package ru;
+
+import java.util.Scanner;
+
+public class HiLo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int theNumber = (int) (Math.random() * 100 + 1);
+    }
+}
---
+++ src/ru/Main.java
@@ -0,0 +1,14 @@
+package ru;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int theNumber = (int) (Math.random() * 100 + 1);
+
+
+	// write your code here
+    }
+}
