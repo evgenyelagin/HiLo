ae5406e7b441e4aab2c09ffe47647cadfe4cb6c9
evgenyelagin <anykeysky@gmail.com>
2020-11-25 18:48:56 +0300

    ver. 1.1 add do-while

--- src/ru/HiLo.java
+++ src/ru/HiLo.java
@@ -7,34 +7,42 @@ public class HiLo {
     public static void main(String[] args) {
         // создаем класс сканер - для принятия значений с клавиатуры
         Scanner scan = new Scanner(System.in);
+        String playAgain = "";
+        // делаем еще один цикл ду-вайл - чтобы можно было повторно сыграть.
+        do {
             /* генерация случайного числа через класс Мас.рандом - фишка в том, что рандом генерит в промежутке
              от 0.0 до 0.9999, поэтому умножаем на 100 - так как в игре надо угадать число от 1 до 100 + 1 чтобы когда
              выпадет ноль случайным образом получить 1 и еще важная штука приведение типа - (int) -
              изменяет тип с десятичной дроби в целое число, отбрасывая десятки */
-        int theNumber = (int) (Math.random() * 100 + 1);
-        // проверка генерации случайного числа чере вывод на экран
-        // System.out.println(theNumber);
-        // объявление и инициализация переменной
-        int guess = 0;
-        // System.out.println("Введи число от 1 до 100 и нажми Enter");
+            int theNumber = (int) (Math.random() * 100 + 1);
+            // проверка генерации случайного числа чере вывод на экран
+            // System.out.println(theNumber);
+            // объявление и инициализация переменной
+            int guess = 0;
+            // System.out.println("Введи число от 1 до 100 и нажми Enter");
         /* сохраним ответ пользователя в переменную guess и применим созданный ранее объект scan, класс Scanner содержит
         метод nextInt(), который ожидает значение типа Инт введенное пользователем.
          */
-        //guess = scan.nextInt();
-        //System.out.println("Вы ввели " + guess + ".");
-        // добавляем цикл, чтобы можно было угадывать бесконечно долго :)
-        while (guess != theNumber) {
-            System.out.println("Введи число от 1 до 100 и нажми Enter");
-            guess = scan.nextInt();
-            System.out.println("Вы ввели " + guess + ".");
-            // делаем условия
-            if (guess < theNumber) {
-                System.out.println(guess + " меньше числа. Попробуй еще раз");
-            } else if (guess > theNumber) {
-                System.out.println(guess + " больше числа. Попробуй еще разок");
-            } else {
-                System.out.println("Ты победил!!!");
-            }
-        }
+            //guess = scan.nextInt();
+            //System.out.println("Вы ввели " + guess + ".");
+            // добавляем цикл, чтобы можно было угадывать бесконечно долго :)
+            while (guess != theNumber) {
+                System.out.println("Введи число от 1 до 100 и нажми Enter");
+                guess = scan.nextInt(); // сканирует ввод с клавиатуры целых чисел
+                System.out.println("Вы ввели " + guess + ".");
+                // делаем условия
+                if (guess < theNumber) {
+                    System.out.println(guess + " меньше числа. Попробуй еще раз");
+                } else if (guess > theNumber) {
+                    System.out.println(guess + " больше числа. Попробуй еще разок");
+                } else {
+                    System.out.println("Ты победил!!!");
+                }
+            } // конец цикла вайл
+            System.out.println("Сыграем еще разок??? (Yes/No)");
+            playAgain = scan.next(); // scan.next сканирует ввод с клавиатуры символов и записывает в playAgain
+        } while (playAgain.equalsIgnoreCase("Yes")); /* сравнивает данную строку с другой строкой,
+        игнорируя регистр. */
     }
 }
+
